5144d512c98c8ffa965dfbfab397af276b55d6ed
erey21 <arinerey@yandex.ru>
2021-11-04 21:42:12 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание № 3");
+            Console.WriteLine("Введите число: ");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number == 5 || number == 10)
+            {
+                Console.WriteLine("Число либо равно 5, либо равно 10.");
+            }
+            else
+            {
+                Console.WriteLine("Неизвестное число");
+            }
+        }
+    }
+}
